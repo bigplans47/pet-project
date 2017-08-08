930cb4bbceb52c14af214975f946af4bcebd9922
Dan Kiss and Riki Montgomrty <riki@>
2017-08-08 14:47:29 -0700

    Add highlight box based on click.

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,9 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $(".box").click(function() {
+    $(".box").addClass("box-background");
+  });
+
 });
